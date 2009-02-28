24357c208e13588ad02f324368925945be51c5cc
eric thomas <eric.l.m.thomas@gmail.com>
2009-02-27 19:13:03 -0500

    Hristo sucks.

--- board.py
+++ board.py
@@ -1,8 +1,10 @@
 # another comment
+# eric test comment
 class Board:
      GOLD = 0x0
      def __init__(self):
          self.board = {}
+	 self.board[0][2] = "X"
          self.nextToMove = self.GOLD;
 	 self.totalMoves = 0;
 
