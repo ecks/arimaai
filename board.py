b9a09eda7ecdc6cd10854ee4e31addf555adb296
et <eric2000and1@yahoo.com>
2009-03-03 18:50:26 -0500

    Added a dictionary. Not final.

--- board.py
+++ board.py
@@ -1,10 +1,35 @@
 # another comment
 # eric2 test comment
 class Board:
+  
+     pieces = {'E' : "Gold Elephant",
+               'e' : "Silver Elephant",
+               
+               'M' : "Gold Camel",
+               'm' : "Silver Camel",
+               
+               'H' : "Gold Horse",
+               'h' : "Silver Horse",
+            
+               'D' : "Gold Dog",
+               'd' : "Silver Dog",
+               
+               'C' : "Gold Cat",
+               'c' : "Silver Cat",
+     
+               'R' : "Gold Rabbit",
+               'r' : "Silver Rabbit"}  
+            
      GOLD = 0x0
      def __init__(self):
          self.board = {}
 	 self.board[0][2] = "X"
-         self.nextToMove = self.GOLD;
-	 self.totalMoves = 0;
+         self.nextToMove = self.GOLD
+	 self.totalMoves = 0
+    
+         print "i";
+
+
+
+board = Board()
 
