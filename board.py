0696d4f903433c1d4492b409c1ff362af1745910
et <eric2000and1@yahoo.com>
2009-03-03 19:18:40 -0500

    Added some comments.

--- board.py
+++ board.py
@@ -1,5 +1,24 @@
 # another comment
 # eric2 test comment
+
+
+# Sample board.
+# 7g 
+#  +-----------------+
+# 8|   r   r r   r   |
+# 7| m   h     e   c |
+# 6|   r x r r x r   |
+# 5| h   d     c   d |
+# 4| E   H         M |
+# 3|   R x R R H R   |
+# 2| D   C     C   D |
+# 1|   R   R R   R   |
+#  +-----------------+
+#    a b c d e d g h
+
+
+
+
 class Board:
   
      pieces = {'E' : "Gold Elephant",
