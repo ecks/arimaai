7415eed748c1df62b706a859fb0712488b2aceff
Hristo Asenov <ecks@myhost.(none)>
2009-03-26 19:04:49 -0700

    made a stupid driver

--- Bot/genMove.py
+++ Bot/genMove.py
@@ -1,3 +1,4 @@
+import sys
 from Position import * 
 
 moveBoardList = []
@@ -5,15 +6,21 @@ moveBoardList = []
 # for board, return a list that contains a tuple of move and board for a particular board state 
 def generateMove(board, turn):
     
+    sys.stderr.write("board <<<<<<<<<<<<\n")
+    sys.stderr.write(str(board)+"\n")
+    sys.stderr.write("board >>>>>>>>>>>>\n")
+    sys.stderr.write("turn <<<<<<<<<<<<<\n")
+    sys.stderr.write(str(turn)+"\n")
+    sys.stderr.write("turn >>>>>>>>>>>>>\n")
     for moveNum in range(0,4): #loop for each move in the turn
         for row in range(0,8): #loop over full board
             for col in range(0,8):
                 if board[row][col].isupper():
-
-                  position = Position(board[row][col] ,row+1, col, 'n')
-                  if checkMove(board, position):
-		     newBoard = updateBoard(board, position)
-                     moveBoardList.add((position, newBoard))
+		  if row != 7 or col != 7:
+                    position = Position(board[row][col] ,row+1, col, 'n')
+                  #  if checkMove(board, position):
+		  #  newBoard = updateBoard(board, position)
+                  #   moveBoardList.add((position, newBoard))
                    
                 
 def checkMove (board, position):
