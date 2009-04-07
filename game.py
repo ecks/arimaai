8b8bc904d05c7ed949eab7cec22784f406ec9804
Hristo Asenov <ecks@myhost.(none)>
2009-04-06 21:56:36 -0700

    deleted unnecessary files

--- game.py
+++
@@ -1,9 +0,0 @@
-class Game:
-
-   def __init__(self, event, site, date, round, gold, silver, result):
-         self.event = event
-         self.site = site
-         self.round = round
-         self.gold = gold
-         self.silver = silver
-         self.result = result
