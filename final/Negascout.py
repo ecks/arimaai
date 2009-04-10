4175d7f9ce0de738dc6f7ba6b0d0433279a46e47
Hristo Asenov <ecks@myhost.(none)>
2009-04-09 18:49:03 -0700

    cleaned up zobrist hashing a bit, everything seems to work

--- final/Negascout.py
+++ final/Negascout.py
@@ -39,12 +39,11 @@ class Negascout(object):
 	    
         if (depth == 0):
             strength = self.eval.evaluateBoard(board, color) #returns the strength value of the board 
-            self.hashkeysEvalsSteps.append((hash.get_hashkey(), strength, steps)) 
-            self.hashkeysEvalsSteps.sort() # warning !!! may not be the best way to do this !!!
+            self.insertEntrySorted((hash.get_hashkey(), strength, steps), self.hashkeysEvalsSteps) 
+            stepsSaved = map(lambda x: x[2], self.hashkeysEvalsSteps)
             return (strength, steps)
         
         b = beta
-        m = ""
 	    
         turnList = []
         
@@ -53,6 +52,9 @@ class Negascout(object):
         # then generate all the possible moves.
         moveGen = MoveGenerator.MoveGenerator(count, color, board, hash)
         # moveGen.genMoves("", lowRow, lowCol, highRow, highCol)
+
+	# make sure that there are no past moves being made, since 
+	# the function will confuse it with push or pull
         moveGen.genMoves("")
         
         # The list of possible moves is stored in moveGen.moveStepHashes
@@ -76,9 +78,7 @@ class Negascout(object):
             
             currentHashKeys = map(lambda x: x[0], self.hashkeysEvalsSteps)
             
-            ins_pt = bisect.bisect_left(currentHashKeys, hashForBoard)
-        
-            if len(currentHashKeys) == ins_pt or hashForBoard != currentHashKeys[ins_pt]:
+	    if not self.isEntryInList(hashForBoard, currentHashKeys):
 	        # original entry, need to reevaluate
             
                 # descend one level and invert the function
@@ -87,18 +87,15 @@ class Negascout(object):
                 a = self.negascout(depth - 1, bTemp, alphaTemp, newBoardState, newColor, stepPerBoard, count, hash)
             else:
                 # already got the evaluation of it, just return the evaluated value
+		ins_pt = self.getInsPt()
                 a = (self.hashkeysEvalsSteps[ins_pt][1],self.hashkeysEvalsSteps[ins_pt][2])
             
             a = (a[0] * -1,a[1])
 
-            print "a: " + str((a,m,b))
 	    # alpha-beta pruning
             if a[0] > alpha[0]:
-                print "Change alpha: " + str(alpha[0]) + " a => " + str(a[0])
                 alpha = a
           
-            print "alpha: " + str((alpha,m,b))
-
             if alpha[0] >= beta[0]:
                 return (alpha[0], steps + " | " + alpha[1])
       
@@ -108,10 +105,32 @@ class Negascout(object):
                 alpha = self.negascout(depth - 1, betaTemp, alphaTemp, newBoardState, newColor, stepPerBoard, count, hash)
                 alpha = (alpha[0] * -1,alpha[1])
 
-                print "in alpha >= b: alpha ==> " + str(alpha)
                 if alpha[0] >= beta[0]:
                     return (alpha[0],steps + " | " + alpha[1])
       
                 b = (alpha[0] + 1,alpha[1])
             
         return (alpha[0],steps + " | " + alpha[1])
+
+    def insertEntrySorted(self, entry, list):
+	    ins_pt = bisect.bisect_left(list, entry)
+	    if len(list) == ins_pt or entry != list[ins_pt]:
+                list.insert(ins_pt, entry)
+	    else:
+                raise Exception, "You are trying to append to list after evaluation, and the entry was found which is impossible!!!"
+      
+    def isEntryInList(self, entry, list):
+	    ins_pt = bisect.bisect_left(list, entry)
+	    if len(list) == ins_pt or entry != list[ins_pt]:
+		self.found_ins_pt = -1
+                return False
+	    else:
+                self.found_ins_pt = ins_pt
+                return True
+
+    def getInsPt(self):
+            if self.found_ins_pt == -1:
+                raise Exception, "Trying to access insertion point when it wasn't defined"
+	    else:
+                return self.found_ins_pt
+ 
